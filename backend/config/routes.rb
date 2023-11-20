89d3a674b630c0d56f35640bdec90b4c51bf095f
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-11-20 02:22:54 +0000

    Add auth user

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'current_user/index'
+  get "/users/current_user", to: "api/v1/users/current_user#index"
+
+  devise_for :users, path: "", path_names: { sign_in: "api/v1/login", sign_out: "api/v1/logout", registration: "api/v1/signup" }, controllers: { sessions: "api/v1/users/sessions", registrations: "api/v1/users/registrations" }
+
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, controller: "users/users", as: "users", path: "users", only: [:index, :show, :create, :update, :destroy]
       resources :activities
       resources :locations
     end
--- backend/db/migrate/20231105152609_create_users.rb
+++ backend/db/migrate/20231105152609_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
     create_table :users do |t|
       t.string :nickname, null: false
       t.string :img, null: false, default: "default_user.png"
-      t.string :discriminator, null: false
+      t.string :discriminator, null: false, default: "Client"
 
       t.timestamps
     end
