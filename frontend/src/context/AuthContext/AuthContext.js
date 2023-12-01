1ed0cb1a0adae4ff6df173f1bc55339fe710d0a0
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-12-01 19:44:49 +0000

    Init UseContext and one use in App Routes

--- frontend/src/context/AuthContext/AuthContext.js
+++ frontend/src/context/AuthContext/AuthContext.js
@@ -1,5 +1,41 @@
-import { createContext } from 'react';
+import {
+  createContext,
+  // useContext, 
+  useState
+} from 'react';
 
-const AuthContext = createContext('NO-AUTHENTICATED');
-const UserContext = createContext(null);
-const RolContext = createContext('visit');
+export const AuthContext = createContext()
+
+export function AuthProvider({ children }) {
+  const [userStatus, setUserStatus] = useState('visit');
+
+  const loginContext = (user) => {
+    if (user.email.exclude('@stillhigher.es')) {
+      setUserStatus('client');
+    } else {
+      if (!user.admin) {
+        setUserStatus('employee');
+      } else {
+        setUserStatus('admin');
+      }
+    }
+  };
+
+  const logoutContext = () => {
+    setUserStatus('visit');
+  };
+
+  return (
+    <AuthContext.Provider value={userStatus}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// const useAuth = () => {
+//   const userContext = useContext(AuthProvider);
+//   return userContext;
+// }
+
+
+// export { AuthProvider, AuthContext}
