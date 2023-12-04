8bdf35f25914b11f8cc42e06de59a2878c7ed028
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-12-04 18:21:39 +0000

    Fix show activity image

--- frontend/src/interfaces/Activities-list/ActivitiesList.jsx
+++ frontend/src/interfaces/Activities-list/ActivitiesList.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { SearchOutlined } from "@ant-design/icons";
 
 import ActivityService from '../../services/Activity/activity.service';
+import { AuthContext } from '../../context/AuthContext/AuthContext';
 
 import './ActivitiesList.css';
 
 
 function ActivitiesList() {
   const [activities, setActivities] = useState([]);
+  const userContext = useContext(AuthContext);
 
   useEffect(() => {
     async function fetchActivities() {
@@ -26,12 +28,12 @@ function ActivitiesList() {
     <div className="activities-container">
       {activities && activities.map((a) => (
         <div className="activity-card" key={a.id}>
-          <div className="img-container"><img src={a.image.url} alt="Imagen descriptiva de la actividad" /></div>
+          <div className="img-container"><img src={a.image ? a.image.url : '/Imgs/default_place.png'} alt="Imagen descriptiva de la actividad" /></div>
           <div className="activity-data">
             <p>{a.name}</p>
             <p>{a.date}</p>
           </div>
-          <p className="par-btn">Ver +</p>
+         {userContext === 'admin' ?  <p className="par-btn">hola</p> :  <p className="par-btn">Ver +</p>}
         </div>
       ))}
       <div className="search-container">
