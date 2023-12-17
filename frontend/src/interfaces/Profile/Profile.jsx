5f32ba856ee79907f9bf2aae00d89130289942e4
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-12-17 23:42:28 +0000

    new changes

---
+++ frontend/src/interfaces/Profile/Profile.jsx
@@ -0,0 +1,172 @@
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext/AuthContext'
+
+import {
+  FileImageFilled
+} from '@ant-design/icons';
+
+import {
+  Form,
+  Switch,
+  message
+} from 'antd';
+
+import Popup from '../../components/PopUp/Popup'
+import './Profile.css';
+import AuthService from "../../services/Auth/auth.service";
+import UserService from "../../services/User/user.service";
+
+function Profile() {
+
+  const nav = useNavigate();
+
+  const [data, setData] = useState({});
+  const [popup, setPopup] = useState(false);
+  const userContext = useContext(AuthContext);
+  const userData = userContext[3];
+  // const setUserData = userContext[4];
+  const [settings, setSettings] = useState(userData.setting);
+
+
+  useEffect(() => {
+    handleUpdateSettings();
+  }, [settings]);
+
+  //Create PopUp for change password
+  const changePassword = () => {
+    let data = {
+      title: 'Introduce tu contraseña y tu nueva contraseña',
+      icon: 'question',
+      input: [
+        {
+          placeholder: 'Contraseña',
+          required: true,
+          type: 'password',
+          error: 'El campo no puede estar vacio'
+        },
+        {
+          placeholder: 'Nueva contraseña',
+          required: true,
+          type: 'password',
+          error: 'El campo no puede estar vacio'
+        }
+      ],
+      button: {
+        method: (val) => {
+          let credentials = btoa(`${val[0]}:${val[1]}`);
+          UserService.updateUserPassword(userData.id, credentials).then(res => {
+            nav('/auth');
+            closePopup()
+          }
+          ).catch(err => console.error(err))
+        },
+        value: 'Cambiar'
+      }
+    }
+    setData(data);
+    setPopup(true);
+  };
+
+  //Creation PopUp for delete account
+  const deleteAccount = () => {
+    let data = {
+      title: '¿Estás seguro/a?',
+      description: 'Está acción no se puede cambiar',
+      icon: 'danger',
+      input: [
+        {
+          placeholder: 'Contraseña',
+          required: true,
+          type: 'password',
+          error: 'El campo no puede estar vacio'
+        }
+      ],
+      button: {
+        method: (val) => {
+          AuthService.deleteAccount().then(res => {
+            nav('/auth');
+            localStorage.removeItem('token');
+            closePopup()
+          }
+          ).catch(err => console.error(err))
+        },
+        value: 'Borrar'
+      }
+    }
+    setData(data);
+    setPopup(true);
+  };
+
+  const closePopup = () => {
+    setPopup(false);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    // setUserData()
+    handleUpdateImage(file);
+  };
+
+  const handleUpdateImage = (image) => {
+    const formData = new FormData();
+    formData.append('user[image]', image);
+    UserService.updateUserImage(userData.id, formData).then(r => console.log(r)).catch(err => console.error(err));
+  }
+
+  const handleInputSettings = (name, value) => {
+    setSettings((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleUpdateSettings = () => {
+    UserService.updateSettings(settings.id, settingsFormData())
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
+
+  const settingsFormData = () => {
+    const formData = new FormData();
+
+    formData.append('setting[font_size]', settings.font_size)
+    formData.append('setting[light_mode]', settings.light_mode)
+    return formData;
+  }
+
+  return (
+    <div className="profile">
+      <form className="form-user">
+        <div className="img-profile">
+          <img src={userData.image ? userData.image.url : '/Imgs/default_user.png'} alt="Foto de perfil de usuario" />
+            <label className="img-label" htmlFor="img-btn">
+            <input id="img-btn" className="img-btn" onChange={handleFileChange} type="file" accept="image/*" multiple={false} />
+            <FileImageFilled className="icon-profile"/>
+            </label>
+        </div>
+        <div className="settings-container">
+          <p className="settings-header">Settings</p>
+          <Form.Item label="Modo luminoso:" valuePropName="checked">
+            <Switch onChange={(checked) => handleInputSettings('light_mode', checked)} checked={settings.light_mode} type="checkbox" name="light_mode" id="lightMode" />
+          </Form.Item>
+          <label htmlFor="normalFontSize">Normal:
+          <input checked={settings.font_size === 'normal'} onChange={() => handleInputSettings('font_size', 'normal')} type="radio" name="font_size" id="normalFontSize" value="normal" />
+          </label>
+          <label htmlFor="BigFontSize">Grande:
+          <input checked={settings.font_size === 'big'} onChange={() => handleInputSettings('font_size', 'big')} type="radio" name="font_size" id="BigFontSize" value="big" />
+          </label>
+        </div>
+      </form>
+      <div className='account-data'>
+        <p onClick={changePassword} className='data-change'>Cambiar contraseña</p>
+        <p onClick={deleteAccount} className='data-change'>Borrar cuenta</p>
+      </div>
+      <div className="popup-container">
+        {popup && <Popup data={data} closePopup={closePopup} />}
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
