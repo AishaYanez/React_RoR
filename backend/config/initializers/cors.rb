834729ec4dde6d2e6cd70165404527c48cff73f9
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-01-12 16:08:08 +0000

    Fix report

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,12 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
-
-    resource "*",
+    resource(
+      "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose: ["access-token", "expiry", "token-type", "Authorization"],
+      methods: [:get, :patch, :put, :delete, :post, :options, :show],
+    )
   end
 end
