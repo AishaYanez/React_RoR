10da4239421ffeaed4b5419845f80bc86f510f6d
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2024-01-15 10:44:03 +0000

    Campo calculado

--- jsreportapp/data/Stillhigher/activities-users-main/helpers.js
+++ jsreportapp/data/Stillhigher/activities-users-main/helpers.js
@@ -1,10 +1,9 @@
-// // Ejemplo de definición del helper en tu script
-// function sumFields(f1, f2) {
-//   return f1 + f2 + 1;
-// }
+function sumFields(f1, f2) {
+  return f1 + f2 + 1;
+}
 
-// module.exports = {
-//   helpers: {
-//       sumFields: sumFields
-//   }
-// };
+module.exports = {
+  helpers: {
+      sumFields: sumFields
+  }
+};
